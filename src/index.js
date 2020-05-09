5ccacd76fe7b328424e20ac4d98920094ce7490d
afstroe <63409056+afstroe@users.noreply.github.com>
2020-05-09 21:24:05 +0100

    first ReactDOM render

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1> Hello world </h1>
+    <p>This will crash the ReactDOM</p>
+  </div>,
+  document.getElementById("root")
+);
