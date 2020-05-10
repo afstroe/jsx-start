6e9a3c15a3287c09f8459f0164898e11156e76af
afstroe <63409056+afstroe@users.noreply.github.com>
2020-05-10 10:46:47 +0100

    inline styles for react components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
+const customStyle = {
+  color: "red",
+  fontSize: "40px",
+  border: "1px solid black"
+};
 ReactDOM.render(
   <div>
-    <h1> Hello world </h1>
+    <h1 style={customStyle}> Hello world </h1>
     <p>This will crash the ReactDOM</p>
   </div>,
   document.getElementById("root")
